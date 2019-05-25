e46973849d18ba21c8e22df5f8417702f188515c
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-25 20:05:42 +0100

    set up posts table

--- db/migrate/20190525182357_create_posts.rb
+++ db/migrate/20190525182357_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :caption
-      t.file :image
+      t.text :image
 
       t.timestamps
     end
