06f095a10367188359956450ff985a0c3b6f5ae0
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-25 23:01:38 +0100

    add postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_182357) do
+ActiveRecord::Schema.define(version: 2019_05_25_203221) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "caption"
