e5434fe0bb9a22178b658e06fe76900b49cc29ff
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-26 15:32:58 +0100

    fix posts routes to add new post to homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,35 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
   end
 
-  def update
-  end
-
-  def edit
+  def index
+    @posts = Post.all
   end
 
   def destroy
+    Post.find(params[:id]).destroy
+    redirect_to posts_url
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/update'
-  get 'posts/edit'
-  get 'posts/destroy'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
