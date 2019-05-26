e0e0af20b0f5ab2ae1e6af9a5b80466c6e0a8454
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-26 15:49:37 +0100

    initiate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
