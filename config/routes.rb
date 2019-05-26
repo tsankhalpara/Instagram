7a00ae8530025e01cf3ee68a8cb21555ff85a96e
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-26 16:42:05 +0100

    add user sign up page with basic css

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
   resources :posts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "users#new"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_203221) do
+ActiveRecord::Schema.define(version: 2019_05_26_145605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_05_25_203221) do
   create_table "posts", force: :cascade do |t|
     t.text "caption"
     t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "email"
+    t.text "fullname"
+    t.text "username"
+    t.text "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
