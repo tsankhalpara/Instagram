9ddbcd9f36d94d6b642616a412071f9c458fcc72
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-27 14:42:09 +0100

    update gems and link sign in to posts page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+      @user.save
+      redirect_to posts_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :fullname, :username, :password)
   end
 end
